84ce38d34f278a4133cca7f573359379ba926bf5
Paito Anderson <pj.paito@gmail.com>
2016-04-21 22:02:38 -0400

    Added support for ASP.Net Core.

---
+++ test/UnitTest/MockClient.cs
@@ -0,0 +1,25 @@
+﻿using SendGrid.CSharp.HTTP.Client;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    // Mock the Client so that we intercept network calls
+    public class MockClient : Client
+    {
+        public MockClient(string host, Dictionary<string, string> requestHeaders = null, string version = null, string urlPath = null) : base(host, requestHeaders, version, urlPath)
+        {
+        }
+
+        public async override Task<Response> MakeRequest(HttpClient client, HttpRequestMessage request)
+        {
+            HttpResponseMessage response = new HttpResponseMessage();
+            response.Content = new StringContent("{'test': 'test_content'}", Encoding.UTF8, "application/json");
+            response.StatusCode = HttpStatusCode.OK;
+            return new Response(response.StatusCode, response.Content, response.Headers);
+        }
+    }
+}
---
+++ test/UnitTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using NUnitLite;
+using System;
+using System.Reflection;
+
+namespace UnitTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+#if DNX451
+            new AutoRun().Execute(args);
+#else
+            new AutoRun().Execute(typeof(Program).GetTypeInfo().Assembly, Console.Out, Console.In, args);
+#endif
+            Console.ReadKey();
+        }
+    }
+}
