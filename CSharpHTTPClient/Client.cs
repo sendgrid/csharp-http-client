67d1d7cb4b1b7d88528cd744e51b4aa9b5f5024e
pandaboy00 <kengie99@hotmail.com>
2016-11-02 11:58:27 -0400

    Added TimeoutDefault value to 10 seconds

--- CSharpHTTPClient/Client.cs
+++ CSharpHTTPClient/Client.cs
@@ -68,11 +68,8 @@ namespace SendGrid.CSharp.HTTP.Client
         public string Version;
         public string UrlPath;
         public string MediaType;
-<<<<<<< HEAD
-        public TimeSpan Timeout; 
-=======
         public TimeSpan Timeout;
->>>>>>> a01c011e11a9159b01b7e9147adb4f612defe718
+
         public enum Methods
         {
             DELETE, GET, PATCH, POST, PUT
@@ -98,11 +95,7 @@ namespace SendGrid.CSharp.HTTP.Client
             }
             Version = (version != null) ? version : null;
             UrlPath = (urlPath != null) ? urlPath : null;
-<<<<<<< HEAD
             Timeout = (timeOut != null) ? (TimeSpan)timeOut : TimeSpan.FromSeconds(TimeoutDefault);
-=======
-            Timeout = (timeOut != null) ? (TimeSpan)timeOut : TimeSpan.FromSeconds(10);
->>>>>>> a01c011e11a9159b01b7e9147adb4f612defe718
         }
 
         /// <summary>
