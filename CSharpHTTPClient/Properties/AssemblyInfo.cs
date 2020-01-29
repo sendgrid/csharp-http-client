0978906876b9f4eccf2c63adda0f59bda5332252
Elmer Thomas <elmer@thinkingserious.com>
2020-01-28 19:29:24 -0800

    update all the things

--- CSharpHTTPClient/Properties/AssemblyInfo.cs
+++ CSharpHTTPClient/Properties/AssemblyInfo.cs
@@ -9,6 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Twilio SendGrid")]
 [assembly: AssemblyProduct("SendGrid.CSharp.HTTP.Client")]
+[assembly: AssemblyCopyright("Copyright Twilio SendGrid 2020")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -17,3 +18,7 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("26c4841f-ec62-4ec7-b16e-3a7386ea36dc")]
+
+[assembly: AssemblyVersion("3.4.2.0")]
+[assembly: AssemblyFileVersion("3.4.2.0")]
+[assembly: AssemblyInformationalVersion("3.4.2")]
